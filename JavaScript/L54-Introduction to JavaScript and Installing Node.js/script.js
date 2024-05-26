f326751363093ba7354ddbe3f2254ebdbc6d8f04
Abhishek Gupta <ag096129abhishek@gmail.com>
2024-05-26 18:42:56 +0530

    Add L54

---
+++ JavaScript/L54-Introduction to JavaScript and Installing Node.js/script.js
@@ -0,0 +1,17 @@
+alert("Hello World !");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is running like a wow...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
+if(isTrue){
+    console.log("Blasting your computer")
+}
+else{
+    console.log("Not blasting your computer")
+}
+document.title = "I am good"
+document.body.style.backgroundColor = "red"
+console.log("Your number is " + a)
