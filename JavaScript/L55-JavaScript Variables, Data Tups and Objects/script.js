5f41e45e37fa8f82e0798f2552d7c102bf64c00c
Abhishek Gupta <ag096129abhishek@gmail.com>
2024-05-27 17:47:32 +0530

    Add L55

---
+++ JavaScript/L55-JavaScript Variables, Data Tups and Objects/script.js
@@ -0,0 +1,41 @@
+console.log("Hello")
+
+let a = 5;
+// a = a + 1;
+let b = 8;
+let c = "Hello";
+// var 55a = "Rohan";  Not Allowed
+
+// console.log(a + b + 40)
+// console.log(typeof a, typeof b, typeof c)
+
+// const a1 =6;
+// a1 = a1 + 1;
+// Not allowed because a1 is constant
+
+{
+    // let a = 66;
+    console.log(a)
+}
+console.log(a)
+
+let x ="harry bahi";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o ={
+    "Name": "Harry",
+    "Jobe Role": 5600,
+    "is_handsome": true
+}
+console.log(o);
+o.salery = "100crores";
+console.log(o)
+o.salery = "500crores";
+console.log(o)
