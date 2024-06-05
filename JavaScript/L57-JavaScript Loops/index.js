47157cb88a06eb35a0df1b863606238b1e0a5c38
Abhishek Gupta <ag096129abhishek@gmail.com>
2024-06-06 01:19:35 +0530

    Add L57

---
+++ JavaScript/L57-JavaScript Loops/index.js
@@ -0,0 +1,37 @@
+console.log("This is Tutorial on loops");
+
+let a=1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+// }
+
+// let obj = {
+//     name: "Harry",
+//     role: "Programmer",
+//     compley: "CodewithHarry AI"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key,element);
+// }
+
+// for (const c of "Harry") {
+//     console.log(c);
+// }
+
+// let i=0;
+// while (i<6) {
+//     console.log(i)
+//     i++;
+// }
+
+let i=0;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
