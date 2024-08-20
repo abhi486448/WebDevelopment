f884b778aa16c389686393fb9fc0e3dd0b04e795
Abhishek Gupta <146754692+abhi486448@users.noreply.github.com>
2024-08-20 05:12:22 -0700

    Add L60

---
+++ JavaScript/L60-JavaScript Strings/index.js
@@ -0,0 +1,27 @@
+console.log("hello this is vedio no. 60");
+let a = "Harry";
+// console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+console.log(a.length);
+
+let real_name = "Harry"
+let friend = "Rohan"
+
+console.log("His name is " + real_name + " and his friends name is " + friend);
+
+console.log(`His name is ${real_name} and his friends name is ${friend}`);
+
+let b = "Shivam"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+console.log(b.replace("Sh","77"))
+console.log(b.concat(a))
+console.log(b.concat(a, "Aishwarya","Rahul","Priya"))
