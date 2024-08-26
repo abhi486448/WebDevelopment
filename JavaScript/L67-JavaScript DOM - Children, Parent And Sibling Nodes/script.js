753368b1049bef1bdd485959548fb9cec419a281
Abhishek Gupta <146754692+abhi486448@users.noreply.github.com>
2024-08-26 12:32:46 -0700

    Add L67

---
+++ JavaScript/L67-JavaScript DOM - Children, Parent And Sibling Nodes/script.js
@@ -0,0 +1,40 @@
+console.log("Hello world")
+
+document.body
+
+document.body.elememnt
+
+document.body.childNodes
+
+document.body.childNodes[0]
+
+document.body.childNodes[1]
+
+document.body.childNodes[1].childNodes
+
+let cont = document.body.childNodes[1]
+
+cont.firstChild
+
+cont.lastChild
+
+cont.firstElementChild
+cont.lastElementChild.style.color = "red"
+cont.lastElementChild.style.backgroundColor = "green"
+cont.lastElementChild.parentNode
+
+document.body.firstElementChild
+
+document.body.firstElementChild.childNodes
+
+document.body.firstElementChild.children
+
+document.body.firstElementChild.children[1]
+
+document.body.firstElementChild.children[2]
+
+document.body.firstElementChild.children[2].nextElementSibling
+
+document.body.firstElementChild.children[2].previousElementSibling
+
+document.body.firstElementChild.children[2].previousSibling
