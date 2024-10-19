9df38ab3300f2d98ec2565911a0f9c287212ac37
Abhishek Gupta <ag096129abhishek@gmail.com>
2024-10-19 18:56:04 +0530

    Add L86

---
+++ Backend/L86-CommonJs Vs EcmaScript Modules/mymodule.js
@@ -0,0 +1,13 @@
+export const a = 1 //named export
+export const b = 2 //named export
+export const c = 3 //named export
+export const d = 4 //named export
+export const e = 6 //named export
+
+const  obj = {
+    x:45,
+    y:78
+
+}
+
+export default obj; // default export
---
+++ Backend/L86-CommonJs Vs EcmaScript Modules/mymodule2.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//     a:34,
+//     b:77
+// }
+
+let c = 56;
+
+module.exports = c;
